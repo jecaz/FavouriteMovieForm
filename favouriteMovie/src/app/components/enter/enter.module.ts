ec0d21b0e8c70b02b7a46b4d86b0eb3aa5b3087d
Jelena Zobenica <jelena.zobenica1@gmail.com>
2021-12-26 12:45:34 +0100

    Add card ng-content component and refactor enter and thank you template

--- favouriteMovie/src/app/components/enter/enter.module.ts
+++ favouriteMovie/src/app/components/enter/enter.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CardModule } from '../card/card.module';
 import { EnterComponent } from './enter.component';
 
 @NgModule({
   declarations: [EnterComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, CardModule],
   providers: [],
   exports: [EnterComponent],
 })
--- favouriteMovie/src/app/components/thank-you/thank-you.module.ts
+++ favouriteMovie/src/app/components/thank-you/thank-you.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { CardModule } from '../card/card.module';
 import { ThankYouComponent } from './thank-you.component';
 
 const routes: Routes = [
@@ -13,7 +14,12 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [ThankYouComponent],
-  imports: [CommonModule, FormsModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+    CardModule,
+  ],
   exports: [ThankYouComponent],
 })
 export class ThankYouModule {}
